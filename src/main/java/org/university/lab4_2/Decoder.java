c0db2d1a1c8cda7257148ba4836359c9735c79ed
alina-melnychukk <alinameln2@gmail.com>
2024-01-07 12:28:14 +0200

    lab 4.2 changed

--- src/main/java/org/university/lab4_2/Decoder.java
+++ src/main/java/org/university/lab4_2/Decoder.java
@@ -1,51 +1,67 @@
 package org.university.lab4_2;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.HashMap;
 
 public class Decoder {
-    //Method for reverse coding
-    public static String reverseDecode(String word) {
-        StringBuilder reversedWord = new StringBuilder(word);
-        return reversedWord.reverse().toString();
-    }
+    public static String decodeVowels(String str) {
 
-    //Method for changing vowels
-    public static String vowelReplaceDecode(String word) {
-        word = word.replaceAll("1", "a");
-        word = word.replaceAll("2", "e");
-        word = word.replaceAll("3", "i");
-        word = word.replaceAll("4", "o");
-        word = word.replaceAll("5", "u");
-        return word;
-    }
+        HashMap<Character, Character> vowels = new HashMap<>();
+        vowels.put('1', 'a');
+        vowels.put('2', 'e');
+        vowels.put('3', 'i');
+        vowels.put('4', 'o');
+        vowels.put('5', 'u');
 
-    //Method for changing consonant
-    public static String consonantReplaceDecode(String word) {
-        StringBuilder decodeWord = new StringBuilder();
-        char prevConsonant = ' ';
+        StringBuilder decoded = new StringBuilder();
 
-        for (char c : word.toCharArray()) {
-            if (Character.isLetter(c)) {
-                if (!isVowel(c)) {
-                    if (prevConsonant == 'z') {
-                        prevConsonant = 'b';
-                    } else {
-                        prevConsonant++;
-                    }
-                    decodeWord.append(prevConsonant);
-                } else {
-                    decodeWord.append(c);
-                }
-            } else {
-                decodeWord.append(c);
+        for (char ch : str.toCharArray()) {
+            if (vowels.containsKey(ch)) {
+                decoded.append(vowels.get(ch));
+            }
+            else {
+                decoded.append(ch);
             }
         }
-        return decodeWord.toString();
+        return decoded.toString();
     }
 
-    //Method to find out if symbol is vowel
-    private static boolean isVowel(char c) {
-        return "aeiou".indexOf(c) != -1;
+    public static String decodeConsonant(String word) {
+        char[] consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k',
+                'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w',
+                'x', 'y', 'z'};
+        StringBuilder result = new StringBuilder(word);
+
+        for (int i = 0; i < result.length(); i++) {
+            char c = result.charAt(i);
+            if (isConsonant(c, consonants)) {
+                char nextConsonant = getNextConsonant(c, consonants);
+                result.setCharAt(i, nextConsonant);
+            }
+        }
+
+        return result.toString();
     }
+
+    public static boolean isConsonant(char c, char[] consonants) {
+        for (char consonant : consonants) {
+            if (c == consonant) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static char getNextConsonant(char c, char[] consonants) {
+        int currentIndex = -1;
+        for (int i = 0; i < consonants.length; i++) {
+            if (c == consonants[i]) {
+                currentIndex = i;
+                break;
+            }
+        }
+
+        int nextIndex = (currentIndex + 1) % consonants.length;
+        return consonants[nextIndex];
+    }
+
 }
--- src/test/java/org/university/lab4_2/DecoderTest.java
+++ src/test/java/org/university/lab4_2/DecoderTest.java
@@ -4,17 +4,49 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class DecoderTest {
     @Test
-    public void testReverseDecode(){
-        assertEquals("testing", Decoder.reverseDecode("gnitset"));
-        assertEquals("hello", Decoder.reverseDecode("olleh"));
-        assertEquals("world", Decoder.reverseDecode("dlrow"));
+    public void testDecodeVowels() {
+        assertEquals("hello", Decoder.decodeVowels("h2ll4"));
+        assertEquals("aeiou", Decoder.decodeVowels("12345"));
+        assertEquals("testing", Decoder.decodeVowels("t2st3ng"));
+        assertEquals("", Decoder.decodeVowels(""));
     }
+
     @Test
-    public void testVowelReplaceDecode() {
-        assertEquals("testing", Decoder.vowelReplaceDecode("t2st3ng"));
-        assertEquals("hello", Decoder.vowelReplaceDecode("h2ll4"));
-        assertEquals("world", Decoder.vowelReplaceDecode("w4rld"));
+    public void testDecodeConsonant() {
+        assertEquals("acdfeg", Decoder.decodeConsonant("abcdef"));
+        assertEquals("XYZ", Decoder.decodeConsonant("XYZ"));
+        assertEquals("", Decoder.decodeConsonant(""));
     }
+
+    @Test
+    public void testIsConsonant() {
+        char[] consonants = {'b', 'c', 'd', 'f', 'g'};
+        assertTrue(Decoder.isConsonant('b', consonants));
+        assertTrue(Decoder.isConsonant('f', consonants));
+        assertFalse(Decoder.isConsonant('a', consonants));
+        assertFalse(Decoder.isConsonant('e', consonants));
+    }
+
+    @Test
+    public void testGetNextConsonant() {
+        char[] consonants = {'b', 'c', 'd', 'f', 'g'};
+        assertEquals('c', Decoder.getNextConsonant('b', consonants));
+        assertEquals('g', Decoder.getNextConsonant('f', consonants));
+        assertEquals('b', Decoder.getNextConsonant('g', consonants));
+        assertEquals('b', Decoder.getNextConsonant('z', consonants));
+    }
+//    @Test
+//    public void testReverseDecode(){
+//        assertEquals("testing", Decoder.reverseDecode("gnitset"));
+//        assertEquals("hello", Decoder.reverseDecode("olleh"));
+//        assertEquals("world", Decoder.reverseDecode("dlrow"));
+//    }
+//    @Test
+//    public void testVowelReplaceDecode() {
+//        assertEquals("testing", Decoder.vowelReplaceDecode("t2st3ng"));
+//        assertEquals("hello", Decoder.vowelReplaceDecode("h2ll4"));
+//        assertEquals("world", Decoder.vowelReplaceDecode("w4rld"));
+//    }
 
 
 
